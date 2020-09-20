c667ef581b61de976da2a02ea62136e051e17784
WantSpeaking <1085896471@qq.com>
2020-09-20 15:52:20 +0800

    read wz

---
+++ Assets/Scripts/Init.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using HaRepacker;
+using MapleLib.WzLib;
+using UnityEngine;
+
+public class Init : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;//用来显示图片
+    public string path = "F:/BaiduYunDownload/079mg5/UI.wz";
+    public string subPath = "UI.wz/Login.img/LoginVer/GameGrade";
+    WzFileManager wzFileManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        wzFileManager = new WzFileManager();
+        var wzFile = wzFileManager.LoadWzFile(path);
+        var wzObject = wzFile.GetObjectFromPath(subPath);
+        //Debug.Log(wzObject?.GetBitmap()?.Width);
+        Debug.LogFormat("Width:{0}\t Height:{1}", wzObject?.GetBitmap()?.Width, wzObject?.GetBitmap()?.Height);
+        spriteRenderer.sprite = TextureToSprite(GetTexrture2DFromPath(wzObject));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public static Texture2D GetTexrture2DFromPath(WzObject wzObject)
+    {
+        var bitMap = wzObject?.GetBitmap();
+        var width = bitMap?.Width ?? 0;
+        var height = bitMap?.Height ?? 0;
+
+        Texture2D t2d = new Texture2D(width, height);
+        t2d.LoadImage(ImageToByte2(bitMap));
+        t2d.Apply();
+        return t2d;
+    }
+
+    private Sprite TextureToSprite(Texture2D tex)
+    {
+        Sprite sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
+        return sprite;
+    }
+
+    public static byte[] ImageToByte2(Image img)
+    {
+        using (var stream = new MemoryStream())
+        {
+            img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+            return stream.ToArray();
+        }
+    }
+}
---
+++ Assets/Scripts/WzFileManager.cs
@@ -0,0 +1,100 @@
+﻿/* Copyright (C) 2015 haha01haha01
+
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+using System;
+using System.Collections.Generic;
+using MapleLib.WzLib;
+using System.IO;
+using MapleLib.WzLib.Util;
+
+namespace HaRepacker
+{
+    public class WzFileManager
+    {
+        #region Constants
+        public static readonly string[] MOB_WZ_FILES = { "Mob", "Mob001", "Mob2" };
+        public static readonly string[] MAP_WZ_FILES = { "Map", "Map001",
+            "Map002", //kms now stores main map key here
+            "Map2" };
+        public static readonly string[] SOUND_WZ_FILES = { "Sound", "Sound001" };
+
+        public static readonly string[] COMMON_MAPLESTORY_DIRECTORY = new string[] {
+            @"C:\Nexon\MapleStory",
+            @"C:\Program Files\WIZET\MapleStory",
+            @"C:\MapleStory",
+            @"C:\Program Files (x86)\Wizet\MapleStorySEA"
+        };
+        #endregion
+
+        private List<WzFile> wzFiles = new List<WzFile>();
+
+        public WzFileManager()
+        {
+        }
+
+        private bool OpenWzFile(string path, WzMapleVersion encVersion, short version, out WzFile file)
+        {
+            try
+            {
+                WzFile f = new WzFile(path, version, encVersion);
+                lock (wzFiles)
+                {
+                    wzFiles.Add(f);
+                }
+                string parseErrorMessage = string.Empty;
+                bool parseSuccess = f.ParseWzFile(out parseErrorMessage);
+                if (!parseSuccess)
+                {
+                    file = null;
+                    //Warning.Error("Error initializing " + Path.GetFileName(path) + " (" + parseErrorMessage + ").");
+                    return false;
+                }
+
+                file = f;
+                return true;
+            }
+            catch (Exception e)
+            {
+                //Warning.Error("Error initializing " + Path.GetFileName(path) + " (" + e.Message + ").\r\nAlso, check that the directory is valid and the file is not in use.");
+                file = null;
+                return false;
+            }
+        }
+        
+        /// <summary>
+        /// Load a WZ file from path
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public WzFile LoadWzFile(string path)
+        {
+            short fileVersion = -1;
+            bool isList = WzTool.IsListFile(path);
+
+            return LoadWzFile(path, WzTool.DetectMapleVersion(path, out fileVersion), fileVersion);
+        }        
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="otherWzFileToLoadAt"></param>
+        /// <param name="path"></param>
+        /// <param name="encVersion"></param>
+        /// <param name="version"></param>
+        /// <param name="panel"></param>
+        /// <param name="currentDispatcher">Dispatcher thread</param>
+        /// <returns></returns>
+        private WzFile LoadWzFile(string path, WzMapleVersion encVersion, short version)
+        {
+            WzFile newFile;
+            if (!OpenWzFile(path, encVersion, version, out newFile))
+            {
+                return null;
+            }
+            return newFile;
+        }       
+    }
+}
